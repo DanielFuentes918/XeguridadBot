35364e87952c292276c44e55138412e7697f2272
Daniel Fuentes <dofuentes@unah.hn>
2024-07-17 13:15:39 -0600

    Mandando inforamcion de 10 unidades por mensaje y ahora solo aparece la placa y no el resto del nombre.

--- Xeguridad_Bot.py
+++ Xeguridad_Bot.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime, timedelta, timezone
+import re
 
 # Configuración de la API de WhatsApp
 WHATSAPP_API_URL = "https://graph.facebook.com/v19.0/354178054449225/messages"
@@ -51,21 +52,16 @@ def obtener_ultima_transmision(unidades):
         }
         response = requests.get(Xeguridad_API_URL, params=params)
         print(f"Estado de la respuesta: {response.status_code}")
-        #print(f"Contenido de la respuesta: {response.text}")
 
         if response.status_code == 200:
             try:
                 transmisiones = response.json()
-                print("transmisiones::>", transmisiones)
                 for data in transmisiones:
-                    print(data['datetime_utc'])
                     # Guardar la última transmisión
                     ultima_transmision = data['datetime_utc']
                     # Imprimir la última transmisión obtenida
                     print(f"Unidad: {unidad['unitnumber']} - Última transmisión: {ultima_transmision}")
-                    # Creo un diccionario dentro del arreglo para poder acceder a ultima_transmision_unidades[0]['unitnumber'], ultima_transmision_unidades[0]['nombre'] etc...
                     ultima_transmision_unidades.append({'unitnumber':unidad['unitnumber'], 'ultima_trans':ultima_transmision, 'nombre':data['name']})
-                    print("ARRAY INFO:", ultima_transmision_unidades[0]['unitnumber'])
             except Exception as e:
                 print(f"Error al procesar las transmisiones: {e}")
 
@@ -76,9 +72,7 @@ def obtener_unidades_no_transmitiendo(ultima_transmision_unidades):
     ahora = datetime.now(timezone.utc)  # Asegurando que 'ahora' tiene información de zona horaria
     
     for unidad in ultima_transmision_unidades:
-        print(unidad['ultima_trans'], "---**---")
         if unidad['ultima_trans']:
-            print("ULTIMA TRANS:", unidad['ultima_trans'])
             try:
                 ultima_transmision_dt = formateando_fecha(unidad['ultima_trans'])
                 
@@ -88,7 +82,6 @@ def obtener_unidades_no_transmitiendo(ultima_transmision_unidades):
                 
                 diferencia = ahora - ultima_transmision_dt
                 if diferencia >= timedelta(hours=24):
-                    print("Diferencia:", diferencia, "--", str(timedelta(hours=24)))
                     unidad['ultima_trans'] = formatear_fecha_dd_mm_aaaa(ultima_transmision_dt)
                     unidades_no_transmitiendo.append(unidad)
             except Exception as e:
@@ -132,13 +125,18 @@ def enviar_mensaje_whatsapp(numero, info_equipos):
     
     return response.status_code
 
+def extraer_placa(nombre):
+    match = re.search(r'\b[A-Z]{3}\d{4}\b', nombre)
+    return match.group(0) if match else nombre  # Retorna el nombre completo si no se encuentra placa
+
 def dividir_en_mensajes(unidades_no_transmitiendo, max_unidades=10):
     mensajes = []
     for i in range(0, len(unidades_no_transmitiendo), max_unidades):
         chunk = unidades_no_transmitiendo[i:i + max_unidades]
         info_equipos = ""
         for unidad in chunk:
-            info_equipos += f"Unidad: {unidad['unitnumber']} | Ultima transmision: {unidad['ultima_trans']} | Datos: {unidad['nombre']} || "
+            placa = extraer_placa(unidad['nombre'])
+            info_equipos += f"Unidad: {placa} con dispositivo: {unidad['unitnumber']} no transmite desde: {unidad['ultima_trans']}"
         mensajes.append(info_equipos)
     return mensajes
 
@@ -146,7 +144,6 @@ def main():
     unidades = obtener_unidades()
     if unidades:
         ultima_transmision_unidades = obtener_ultima_transmision(unidades)
-        print("ultima_transmision_unidades::", ultima_transmision_unidades)
         unidades_no_transmitiendo = obtener_unidades_no_transmitiendo(ultima_transmision_unidades)
         if unidades_no_transmitiendo:
             mensajes = dividir_en_mensajes(unidades_no_transmitiendo)
