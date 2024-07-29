c4b686cd0a53cb82262a5e69280d6c8ffaf1ec19
Daniel Fuentes <dofuentes@unah.hn>
2024-07-29 14:12:16 -0600

    cambio de puerto

--- Politicas_Seguridad_Flask.py
+++ Politicas_Seguridad_Flask.py
@@ -7,4 +7,4 @@ def politica_privacidad():
     return render_template('PoliticasSeguridad.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
