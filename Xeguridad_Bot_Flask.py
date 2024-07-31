9b4d2ba93625c1e300b32a8b0999ad25c003f294
Daniel Fuentes <dofuentes@unah.hn>
2024-07-31 11:19:50 -0600

    implementacion de plantilla solicitud_unidad_comandos

--- Xeguridad_Bot_Flask.py
+++ Xeguridad_Bot_Flask.py
@@ -9,6 +9,7 @@ WHATSAPP_API_URL = "https://graph.facebook.com/v19.0/354178054449225/messages"
 WHATSAPP_API_TOKEN = "EAAFiQXfoAV4BO10PdMbULG2wAmGa108puKpkvVzOzWiSMAusEp4xinrQ8DqcORjWZCzQ07DlNIR3jrcsNGbHVFx0zaJOOzn0GurZC0aTCATmCarHUgne5wWhdNp7qDQvpRMZBwFeWOOWC5ZCDpkmfjRUCMG5s51w4YlB7w1XZBdOgqQfENknQ4XdNsNWHQsZBGSQZDZD"
 NAMESPACE = "Xeguridad"
 MENU_TEMPLATE_NAME = "menu2_xeguridad"  # Asegúrate de que este nombre coincida con el de tu plantilla de menú
+SOLICITUD_UNIDAD_COMANDOS_TEMPLATE_NAME = "solicitud_unidad_comandos"  # Nombre de la plantilla para solicitud de comandos a unidad
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
@@ -38,8 +39,26 @@ def webhook():
 def manejar_mensaje_entrante(mensaje):
     print(f"Manejando mensaje entrante: {mensaje}")
     numero = mensaje['from']
+    tipo = mensaje['type']
+
+    if tipo == 'interactive':
+        interaccion = mensaje['interactive']
+        tipo_interaccion = interaccion['type']
+        
+        if tipo_interaccion == 'button_reply':
+            id_respuesta = interaccion['button_reply']['id']
+
+            if id_respuesta == 'mandar_comandos_a_unidad':
+                manejar_respuesta_usuario(numero, SOLICITUD_UNIDAD_COMANDOS_TEMPLATE_NAME)
+                return
+
     components = []  # No enviar parámetros si la plantilla no los espera
     response_status = enviar_mensaje_whatsapp(numero, MENU_TEMPLATE_NAME, components)
+    print(f"Estado de la respuesta al enviar mensaje: {response_status}")
+
+def manejar_respuesta_usuario(numero, template_name):
+    components = []  # Añadir los parámetros necesarios si los hay
+    response_status = enviar_mensaje_whatsapp(numero, template_name, components)
     print(f"Estado de la respuesta al enviar mensaje: {response_status}")
 
 def enviar_mensaje_whatsapp(numero, template_name, components):
@@ -76,6 +95,7 @@ def politica_privacidad():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
 
 
 
