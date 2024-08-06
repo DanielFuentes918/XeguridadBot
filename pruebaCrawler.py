70a41beccac0fac2b3ee43fe16a0782daf84153d
Daniel Fuentes <dofuentes@unah.hn>
2024-08-06 07:55:37 -0600

    actualizacion de options

--- pruebaCrawler.py
+++ pruebaCrawler.py
@@ -13,8 +13,10 @@ def execute_crawler(unitnumber):
     options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--headless')  # Ejecutar en modo sin cabeza
-    options.add_argument('--disable-gpu')  # Deshabilitar GPU para mejorar la estabilidad en modo sin cabeza
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--remote-debugging-port=9222')
+    options.add_argument('--disable-software-rasterizer')
     options.binary_location = '/usr/bin/chromium-browser' # Ajusta esta ruta a la ubicación de tu binario de Chrome
 
     # Inicializar el webdriver
