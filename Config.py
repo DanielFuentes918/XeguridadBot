e5aaaaee485d2cfa8bbacb3da160f71231b51125
Daniel Fuentes <dofuentes@unah.hn>
2024-12-18 13:14:36 -0600

    cambio de nombres en plantilla config

--- Config.py
+++ Config.py
@@ -32,12 +32,10 @@ class Config:
         self.RESPUESTA_COMANDOS_TEMPLATE = "lt_command__response"
         self.COMANDO_NO_RECIBIDO_TEMPLATE = "lt_command__failed"
         self.PLACA_NO_ENCONTRADA_TEMPLATE = "plate_number_wasnt_find"
-        self.COMPLAINT_CLAIMS_TEMPLATE = "complaint_claims_request"
+        self.COMPLAINT_CLAIMS_TEMPLATE = "complaint_claims_requested"
         self.COMPLAINT_CLAIMS_COPY_TEMPLATE = "complaint_claims_copy"
-        self.COMPLAINT_CLAIMS_NOTIFICATION_TEMPLATE = "complaint_claims_notification"
+        self.COMPLAINT_CLAIMS_NOTIFICATION_TEMPLATE = "complaint_claims_confirmation"
         self.COMPANY_SELECTION_TEMPLATE = "company_selection"
 
     def mongo_uri(self):
         return f"mongodb://{self.USUARIO_MONGO}:{self.PASSWORD_MONGO_ESCAPADA}@localhost:27017/{self.BASE_DATOS_MONGO}?authSource={self.AUTH_DB}"
-
-        
