aaa38665b92971493f1c8c45a4a42a295bf40194
Daniel Fuentes <dofuentes@unah.hn>
2025-01-04 08:50:51 -0600

    refactor: change conditional structure for plate request handling in manejar_mensaje_entrante function

--- Xeguridad_Bot_Flask.py
+++ Xeguridad_Bot_Flask.py
@@ -195,7 +195,7 @@ def manejar_mensaje_entrante(mensaje):
             if numero in esperando_plate_request:
                 envioTemplateTxt(numero, config.SOLICITUD_UNIDAD_COMANDOS_TEMPLATE_NAME, [])
                 esperando_placa[numero] = True
-            elif esperando_placa.get(numero):
+            if esperando_placa.get(numero):
                 placa = cuerpo_mensaje.upper()
                 print(f"Placa detectada: {placa}")
                 unitnumber = buscar_unitnumber_por_placa(placa)
