459409f647726c3572e7defabc61f3fc1d9ecf66
Daniel Fuentes <dofuentes@unah.hn>
2025-01-16 23:29:37 -0600

    fix: correct filter syntax in get_trucks_for_user function and update UserAllowedTrucks model fields to String type

--- models/UserAllowedTrucks.py
+++ models/UserAllowedTrucks.py
@@ -6,11 +6,11 @@ class UserAllowedTrucks(db.Model):
     __tablename__ = "vwUserAllowedTrucks"  # Nombre de la vista en la base de datos
     __table_args__ = {"schema": "exa"}  # Esquema en el que se encuentra la vista
 
-    userId = db.Column(db.Integer, primary_key=True)  # Identificador único del usuario
-    userEmail = db.Column(db.Text, nullable=False)  # Cambiado a Text
-    allowedSubdivisionId = db.Column(db.Integer, nullable=False)  # ID de la subdivisión permitida
-    truckId = db.Column(db.Integer, nullable=False)  # ID del camión
-    truckPlate = db.Column(db.Text, nullable=False)  # Cambiado a Text
-    subdivisionName = db.Column(db.Text, nullable=False)  # Cambiado a Text
-    brand = db.Column(db.Text, nullable=True)  # Cambiado a Text
-    model = db.Column(db.Text, nullable=True)  # Cambiado a Text
+    userId = db.Column(db.Integer, primary_key=True)  # Parte de la clave primaria
+    allowedSubdivisionId = db.Column(db.Integer, primary_key=True)  # Parte de la clave primaria
+    truckId = db.Column(db.Integer, primary_key=True)  # Parte de la clave primaria
+    userEmail = db.Column(db.String(255), nullable=False)
+    truckPlate = db.Column(db.String(255), nullable=False)
+    subdivisionName = db.Column(db.String(255), nullable=False)
+    brand = db.Column(db.String(255), nullable=True)
+    model = db.Column(db.String(255), nullable=True)
