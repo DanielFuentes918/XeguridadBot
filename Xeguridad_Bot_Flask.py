8fe9750ed5420b83a95f3daf84d76a11530521a1
Daniel Fuentes <dofuentes@unah.hn>
2025-01-16 23:17:19 -0600

    fix: disable SQLALCHEMY_TRACK_MODIFICATIONS to reduce overhead

--- Xeguridad_Bot_Flask.py
+++ Xeguridad_Bot_Flask.py
@@ -34,7 +34,7 @@ autenticado = {}
 app = Flask("Xeguridad_Bot_Main")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config.SQLALCHEMY_TRACK_MODIFICATIONS
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 # Conexion a MongoDB con manejo de excepciones
